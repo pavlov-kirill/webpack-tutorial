b46e5d9bb451f6c1d1d7ecf15dca935d4ed87d6e
Kirill Pavlov <Stricker1990@gmail.com>
2024-03-16 15:43:01 +0400

    added scss

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,10 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
             }
         ]
     },
